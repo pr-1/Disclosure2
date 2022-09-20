744ef5c18d880724687f57759180708062e22054
alanluckett <alan@nxav.co.uk>
2022-09-20 01:31:37 +0100

    change top image from svg to png to test

--- navigation/ShopTabNavigator.js
+++ navigation/ShopTabNavigator.js
@@ -38,7 +38,7 @@ function LogoTitle() {
   return (
     <Image
       style={{ width: 50, height: 50 }}
-      source={require("../assets/icons/Home_icon.svg")}
+      source={require("../assets/icons/Home_icon.png")}
     />
   );
 }
